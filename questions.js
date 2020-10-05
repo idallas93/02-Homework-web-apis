955eba0efff66f6e7c7f4a078e25f2fbf2d81c91
Isaac Dallas <Dallas.Isaac1@gmail.com>
2020-10-05 19:14:53 -0400

    updated quiz questions to include JS questions and answers

---
+++ questions.js
@@ -0,0 +1,39 @@
+var questions = [
+  {
+      title: "Inside which HTML element do we put JavaScript?",
+      choices: ["<javascript>", "<scripting>", "<script>", "<js>"],
+      answer: "<script>"
+  },
+
+  {
+      title: "What is the correct JavaScript syntax to change the content of the HTML element below?",
+      choices: ["#demo.innerHTML = 'Hello World!';", "document.getElementByName('p').innerHTML = 'Hello World!';", "document.getElementById('demo').innerHTML = 'Hello World!';", "document.getElement('p').innerHTML = 'Hello World!'"],
+      answer: "Hen of the Woods"
+  },
+
+  {
+      title: "What is the correct place to insert a JavaScript",
+      choices: ["The <body> section", "The <head> section", "The <head> and the <body> section", "up your butt"],
+      answer: "The <body> section"
+  },
+
+  {
+      title: "What is the correcy syntax for referring to an external script called 'xxx.js'",
+      choices: ["<script src ='xxx.js'>", "<script name='xxx.js'", "<script href='xxx.js'", "your mom"],
+      answer: "<script src ='xxx.js'>"
+  },
+
+  {
+      title: "How do you write 'Hello World' in an alert box?",
+      choices: ["alert('Hello World');", "msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');"],
+      answer: "alert('Hello World');"
+  },
+
+  {
+      title: "How do you create a function in JavaScript?",
+      choices: ["function = myFunction", "function myFunction()", "function:myFunction()", "function(yourFunction)-myFunction"],
+      answer: "function myFunction()"
+  },
+
+
+];
